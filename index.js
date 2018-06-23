a28fc2f06ef77b29bc5243a104993fa66df09563
haotian <ritou11@gmail.com>
2018-06-23 17:12:27 +0800

    output to file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const _ = require('lodash');
 const axios = require('axios');
 const jsdom = require('jsdom');
 const xlsx = require('xlsx');
+const fs = require('fs');
 
 const { JSDOM } = jsdom;
 
@@ -25,20 +26,20 @@ const query = async (ksh, xm) => {
 };
 
 const invoke = (data, i, maxL) => {
-    query(data[i]['考生号'].trim(), data[i]['姓名'].trim()).then((ct) => {
-      console.log(data[i]['姓名'], ct);
-      if(i < maxL - 1) _.delay(invoke, 1000, data, i+1, maxL);
-    }).catch((error) => {
-      console.log(error.code);
-      if(i < maxL - 1) _.delay(invoke, 1000, data, i+1, maxL);
-    });
-}
+  query(data[i]['考生号'].trim(), data[i]['姓名'].trim()).then((ct) => {
+    console.log(`${data[i]['姓名']}, ${parseInt(ct, 10)}, ${i + 1}/${maxL}`);
+    fs.appendFileSync('./res.txt', `${data[i]['姓名']},${parseInt(ct, 10)}\n`);
+    if (i < maxL - 1) _.delay(invoke, 300, data, i + 1, maxL);
+  }).catch(() => {
+    // console.log(error.code);
+    if (i < maxL - 1) _.delay(invoke, 300, data, i, maxL);
+  });
+};
 
 const workbook = xlsx.readFile('data.xlsx');
 const snl = workbook.SheetNames;
 const xlData = xlsx.utils.sheet_to_json(workbook.Sheets[snl[0]]);
 invoke(xlData, 0, xlData.length);
+
 // console.log(dt['考生号'].trim(), dt['姓名'].trim());
-
-
 // query(17220104153346, '王天贺');
