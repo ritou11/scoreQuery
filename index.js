8c718c5e49cce376202174a74ea9a63a8dc07c59
haotian <ritou11@gmail.com>
2018-06-23 15:05:03 +0800

    get all

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
+const _ = require('lodash');
 const axios = require('axios');
-const jsdom = require("jsdom");
-const fs = require('fs');
-const parse = require('csv-parse');
+const jsdom = require('jsdom');
+const xlsx = require('xlsx');
+
 const { JSDOM } = jsdom;
 
 const ax = axios.create({
-    baseURL: 'http://q3.jletv.cn/',
-    timeout: 1000,
+  baseURL: 'http://q3.jletv.cn/',
+  timeout: 2000,
 });
 
 const query = async (ksh, xm) => {
-    const { data } = await ax.get('wechat/gk-query.php', {
-        params: {
-            ksh,
-            xm,
-            message: '',
-        }
-    });
-    const dom = new JSDOM(data);
-    const { textContent } = dom.window.document.querySelector('body > div > div.main.main-search.main-gk > div > div > div > table > tbody > tr:nth-child(6) > td:nth-child(2)');
-    console.log(parseInt(textContent));
+  const { data } = await ax.get('wechat/gk-query.php', {
+    params: {
+      ksh,
+      xm,
+      message: '',
+    },
+  });
+  const dom = new JSDOM(data);
+  const cell = dom.window.document.querySelector('body > div > div.main.main-search.main-gk > div > div > div > table > tbody > tr:nth-child(6) > td:nth-child(2)');
+  if (cell) return cell.textContent;
+  return 0;
 };
 
-query(17220104153346, '王天贺');
+const invoke = (data, i, maxL) => {
+    query(data[i]['考生号'].trim(), data[i]['姓名'].trim()).then((ct) => {
+      console.log(data[i]['姓名'], ct);
+      if(i < maxL - 1) _.delay(invoke, 1000, data, i+1, maxL);
+    }).catch((error) => {
+      console.log(error.code);
+      if(i < maxL - 1) _.delay(invoke, 1000, data, i+1, maxL);
+    });
+}
+
+const workbook = xlsx.readFile('data.xlsx');
+const snl = workbook.SheetNames;
+const xlData = xlsx.utils.sheet_to_json(workbook.Sheets[snl[0]]);
+invoke(xlData, 0, xlData.length);
+// console.log(dt['考生号'].trim(), dt['姓名'].trim());
+
+
+// query(17220104153346, '王天贺');
