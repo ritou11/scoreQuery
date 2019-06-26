36cb4580332d042c5bd68f892db3457a97c59ebf
haotian <ritou11@gmail.com>
2019-06-26 13:40:35 +0800

    upgrade 2019

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const jsdom = require('jsdom');
 const xlsx = require('xlsx');
 const fs = require('fs');
 
+const { log } = console;
 const { JSDOM } = jsdom;
 
 const ax = axios.create({
-  baseURL: 'http://q3.jletv.cn/',
+  baseURL: 'http://q1.jletv.cn/',
   timeout: 2000,
 });
 
@@ -20,15 +21,33 @@ const query = async (ksh, xm) => {
     },
   });
   const dom = new JSDOM(data);
-  const cell = dom.window.document.querySelector('body > div > div.main.main-search.main-gk > div > div > div > table > tbody > tr:nth-child(6) > td:nth-child(2)');
-  if (cell) return cell.textContent;
-  return 0;
+  const ywCell = dom.window.document.querySelector('body > div > div.main.main-search.main-gk > div > div > div > table > tbody > tr:nth-child(1) > td:nth-child(2)');
+  const sxCell = dom.window.document.querySelector('body > div > div.main.main-search.main-gk > div > div > div > table > tbody > tr:nth-child(2) > td:nth-child(2)');
+  const zhCell = dom.window.document.querySelector('body > div > div.main.main-search.main-gk > div > div > div > table > tbody > tr:nth-child(3) > td:nth-child(2)');
+  const wyCell = dom.window.document.querySelector('body > div > div.main.main-search.main-gk > div > div > div > table > tbody > tr:nth-child(4) > td:nth-child(2)');
+  const mzCell = dom.window.document.querySelector('body > div > div.main.main-search.main-gk > div > div > div > table > tbody > tr:nth-child(5) > td:nth-child(2)');
+  const zfCell = dom.window.document.querySelector('body > div > div.main.main-search.main-gk > div > div > div > table > tbody > tr:nth-child(6) > td:nth-child(2)');
+  if (zfCell) {
+    return {
+      zf: parseInt(zfCell.textContent, 10),
+      yw: parseInt(ywCell.textContent, 10),
+      sx: parseInt(sxCell.textContent, 10),
+      wy: parseInt(wyCell.textContent, 10),
+      zh: parseInt(zhCell.textContent, 10),
+      mz: parseInt(mzCell.textContent, 10),
+    };
+  }
+  return {
+    zf: 0,
+  };
 };
 
 const invoke = (data, i, maxL) => {
-  query(data[i]['考生号'].trim(), data[i]['姓名'].trim()).then((ct) => {
-    console.log(`${data[i]['姓名']}, ${parseInt(ct, 10)}, ${i + 1}/${maxL}`);
-    fs.appendFileSync('./res.txt', `${data[i]['姓名']},${parseInt(ct, 10)}\n`);
+  const csh = data[i]['考生号'].trim();
+  const xm = data[i]['姓名'].trim();
+  query(csh, xm).then((cj) => {
+    log(`${data[i]['姓名']}, ${cj.zf}, ${i + 1}/${maxL}`);
+    fs.appendFileSync('./res.txt', `${csh},${xm},${cj.yw},${cj.sx},${cj.zh},${cj.wy},${cj.mz},${cj.zf}\n`);
     if (i < maxL - 1) _.delay(invoke, 300, data, i + 1, maxL);
   }).catch(() => {
     // console.log(error.code);
@@ -39,6 +58,7 @@ const invoke = (data, i, maxL) => {
 const workbook = xlsx.readFile('data.xlsx');
 const snl = workbook.SheetNames;
 const xlData = xlsx.utils.sheet_to_json(workbook.Sheets[snl[0]]);
+// invoke(xlData, 0, 2);
 invoke(xlData, 0, xlData.length);
 
 // console.log(dt['考生号'].trim(), dt['姓名'].trim());
